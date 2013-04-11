a8480cc10ae6b44a0ad1795852833d6b88989be5
Marcus Falgert <marcus.falgert@gmail.com>
2013-04-11 14:41:00 +0200

    playbar layout

---
+++ Soundz/src/com/example/soundz/Trackbar.java
@@ -0,0 +1,20 @@
+package com.example.soundz;
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Trackbar extends Fragment
+{
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+	{
+		super.onCreateView(inflater, container, savedInstanceState);
+		View view = inflater.inflate(R.layout.trackbar, container, false);
+        return view;
+	}
+	
+}
