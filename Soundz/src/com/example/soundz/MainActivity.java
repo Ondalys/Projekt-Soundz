c91bff72479c4033387c899a327073d2bf9ac57a
Marcus Falgert <marcus.falgert@gmail.com>
2013-04-11 11:25:06 +0200

    gui,fragments

--- Soundz/src/com/example/soundz/MainActivity.java
+++ Soundz/src/com/example/soundz/MainActivity.java
@@ -2,11 +2,14 @@ package com.example.soundz;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentTransaction;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends Activity
 {
-
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
@@ -17,9 +20,32 @@ public class MainActivity extends Activity
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu)
 	{
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+		MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return true;
 	}
+	
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+	    switch(item.getItemId())
+	    {
+	    	case R.id.action_settings:
+		    	Fragment fragment = new Fragment();
+		    	setContentView(R.layout.activity_settings);
+		    	if(findViewById(R.id.linearLayout2) != null) {
+		            fragment.setArguments(getIntent().getExtras());
+		            FragmentTransaction transaction = getFragmentManager().beginTransaction();
+		            transaction.replace(R.id.linearLayout2, fragment);
+		            transaction.addToBackStack(null);
+		            transaction.commit();
+		        }
+	    }
 
+	    	return true;
+	}
+	
+	@Override
+	public void onBackPressed(){
+	    
+	}
 }
